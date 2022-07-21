291efccddac51446d21e910c45d9c59ecabfda60
Igor <dont@bzzr.me>
2022-07-21 22:42:18 +0100

    feature(navigation): add responsive navigation

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 const postcssImport = require("postcss-import");
 const postcssPresetEnv = require("postcss-preset-env");
 const autoprefixer = require("autoprefixer");
+const postcssCustomMedia = require("postcss-custom-media");
 const cssNano = require("cssnano");
 
 module.exports = {
@@ -8,8 +9,7 @@ module.exports = {
 		postcssImport(),
 		postcssPresetEnv(),
 		autoprefixer(),
-		cssNano({
-			preset: "default"
-		})
+		cssNano({ preset: "default"	}),
+    postcssCustomMedia(),
 	]
 }
